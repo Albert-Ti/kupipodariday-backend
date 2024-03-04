a7d4af1ee72ce46a72549f23139418380dff0701
Albert Taygibov <albert.ti.2017@yandex.ru>
2024-03-04 16:55:44 +0300

    feat: creating and connecting to a database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: parseInt(process.env.POSTGRES_PORT, 10),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_USER_PASSWORD,
+      database: process.env.POSTGRES_NAME_DB,
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
-  providers: [],
 })
 export class AppModule {}
